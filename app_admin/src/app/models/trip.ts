07cc7f0175fef134fa1ce1b0b58fe0e20c362a26
Mohamed Elmarzougui <mohamed1elmarzougui@gmail.com>
2025-06-14 21:40:22 -0500

    Module 6 Completed Baseline

---
+++ app_admin/src/app/models/trip.ts
@@ -0,0 +1,11 @@
+export interface Trip{
+    _id: string,
+    code: string,
+    name: string,
+    lenght: string,
+    start: Date,
+    resort: string,
+    perPerson: string,
+    image: string,
+    description: string
+}
